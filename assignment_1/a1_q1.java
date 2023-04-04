584664e0fd10bd8151fbe84eb26593dff1d70464
ANKIT SUBHAMJYOTI <83970700+ankitsubhamjyoti2005@users.noreply.github.com>
2023-04-04 20:30:12 +0530

    Create a1_q1.java

---
+++ assignment_1/a1_q1.java
@@ -0,0 +1,13 @@
+public class a1_q1 {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int c = 1;
+        int count = 0 ;
+        while(a>c){
+            c=c*2;
+            count++;
+        }
+        int g = count - 2 ;
+        System.out.println(g);
+    }
+}
